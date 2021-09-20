33e1868d7f881709996f3a9f8edc25bc3e999ac7
mehedisaurav <mehedi_60@ymail.com>
2021-09-20 10:59:57 +0600

    service class change

---
+++ NotificationLibrary/NotificationServices/INotificationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationLibrary.NotificationServices
+{
+    public abstract class INotificationService
+    {
+        public abstract Task EmailService();
+
+        public abstract Task SmsService();
+
+        public abstract Task WebPushService();
+    }
+}
---
+++ NotificationLibrary/NotificationServices/NotificationService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationLibrary.NotificationServices
+{
+    public class NotificationService : INotificationService
+    {
+        public async override Task EmailService()
+        {
+            Console.WriteLine("");
+        }
+
+        public async override Task SmsService()
+        {
+            Console.WriteLine("");
+        }
+
+        public async override Task WebPushService()
+        {
+            Console.WriteLine("");
+        }
+
+
+    }
+}
