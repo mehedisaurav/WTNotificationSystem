afe811174aa3d06d785c67ba8fc18b29b682099d
mehedisaurav <mehedi_60@ymail.com>
2021-09-20 10:57:13 +0600

    minor change

--- NotificationLibrary/Application/ManageLeave/Commands/LeaveManagementCommand.cs
+++ NotificationLibrary/Application/ManageLeave/Commands/LeaveManagementCommand.cs
@@ -1,36 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NotificationLibrary.Application.Common;
 using NotificationLibrary.Infrastructure.Entities;
+using NotificationLibrary.NotificationServices;
 using NotificationSystem.Infrastructure;
-using NotificationSystem.NotificationServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace NotificationLibrary.Application.ManageLeave.Commands
 {
     public class LeaveManagementCommand
     {
         private readonly INotificationSystemDbContext _dbContext;
-        private readonly IEmailService _emailService;
-        private readonly ISmsService _smsService;
-        private readonly IWebPushService _webPushService;
+        private readonly NotificationService _service;
 
-        public LeaveManagementCommand() { }
-        public LeaveManagementCommand(INotificationSystemDbContext dbContext,
-                                        IEmailService emailService,
-                                        ISmsService smsService,
-                                        IWebPushService webPushService)
+        public LeaveManagementCommand()
+        {
+
+        }
+        public LeaveManagementCommand(INotificationSystemDbContext dbContext, NotificationService service)
         {
             _dbContext = dbContext;
-            _emailService = emailService;
-            _smsService = smsService;
-            _webPushService = webPushService;
+            _service = service;
         }
 
         // Employee request for a leave
-        public async void EmployeeLeaveRequest(int employeeId)
+        public async Task EmployeeLeaveRequest(int employeeId)
         {
             var employee = await _dbContext.Employees.Include(x => x.User).FirstOrDefaultAsync(x => x.Id == employeeId);
             if (employee != null)
@@ -48,26 +45,29 @@ namespace NotificationLibrary.Application.ManageLeave.Commands
                 if (employee.User.NotificationPreferences.Split(',').ToList().Contains(NotificationPreferencesType.Email.ToString()))
                 {
                     // Find manager detail then send to manager email addrees
-                    await _emailService.EmailSender("Manager email address", $"Dear Manager, Mr. {employee.FirstName} request for a leave.");
+                    //await _emailService.EmailSender("Manager email address", $"Dear Manager, Mr. {employee.FirstName} request for a leave.");
+                     await _service.EmailService();
                 }
 
                 // cheking preference
                 if (employee.User.NotificationPreferences.Split(',').ToList().Contains(NotificationPreferencesType.Sms.ToString()))
                 {
                     // Find manager detail then send to manager sms
-                    await _smsService.SmsSender("Manager phone number", $"Mr. {employee.FirstName} request for a leave.");
+                    //await _smsService.SmsSender("Manager phone number", $"Mr. {employee.FirstName} request for a leave.");
+                    await _service.SmsService();
                 }
 
                 // cheking preference
                 if (employee.User.NotificationPreferences.Split(',').ToList().Contains(NotificationPreferencesType.WebPush.ToString()))
                 {
                     //Web push service
+                    await _service.WebPushService(); 
                 }
             }
         }
 
         // Leave request approved/declined
-        public async void LeaveApproval(int leaveId, int leaveApprovalType)
+        public async Task LeaveApproval(int leaveId, int leaveApprovalType)
         {
             var leave = _dbContext.Leaves.Include(x => x.Employee)
                                          .ThenInclude(x => x.User)
@@ -85,22 +85,25 @@ namespace NotificationLibrary.Application.ManageLeave.Commands
                 if (leave.Employee.User.NotificationPreferences.Split(',').ToList().Contains(NotificationPreferencesType.Email.ToString()))
                 {
                     //Email notification send to employee for leave approval status
-                    await _emailService.EmailSender(leave.Employee.Email,
-                                                $"Dear {leave.Employee.FirstName}, Your leave request has been " +
-                                                $"{(leaveApprovalType == LeaveProcessType.Approved ? "Approved" : "Declined")}.");
+                    //await _emailService.EmailSender(leave.Employee.Email,
+                    //                            $"Dear {leave.Employee.FirstName}, Your leave request has been " +
+                    //                            $"{(leaveApprovalType == LeaveProcessType.Approved ? "Approved" : "Declined")}.");
+                    await _service.EmailService();
                 }
 
                 // cheking preference
                 if (leave.Employee.User.NotificationPreferences.Split(',').ToList().Contains(NotificationPreferencesType.Sms.ToString()))
                 {
                     //Sms notification send to employee for leave approval status
-                    await _smsService.SmsSender(leave.Employee.PhoneNumber, $"Your leave request has been {(leaveApprovalType == LeaveProcessType.Approved ? "Approved" : "Declined")}.");
+                    //await _smsService.SmsSender(leave.Employee.PhoneNumber, $"Your leave request has been {(leaveApprovalType == LeaveProcessType.Approved ? "Approved" : "Declined")}.");
+                    await _service.SmsService();
                 }
 
                 // cheking preference
                 if (leave.Employee.User.NotificationPreferences.Split(',').ToList().Contains(NotificationPreferencesType.WebPush.ToString()))
                 {
                     //Web push service
+                    await _service.WebPushService();
                 }
             }
         }
